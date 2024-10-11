1287e0aa2786e5230808be635d6f4db0fec91954
Fan Zhang <96807504+zfkarl@users.noreply.github.com>
2024-10-11 14:03:49 +0800

    Update finetune_cite.sh

--- finetune_cite.sh
+++ finetune_cite.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python train_cite.py --lr 1e-4 --batch_size 256 --checkpoint '/data2/zeyu/zeyu1/scLEAF-v2/results/cite-asap/1_False_500_0.00015_cite/lightning_logs/checkpoints/last.ckpt' >res/cite_small_finetune_last.txt
+CUDA_VISIBLE_DEVICES=3 python train_cite.py --lr 1e-4 --batch_size 256 --checkpoint './results/cite-asap/1_False_500_0.00015_cite/lightning_logs/checkpoints/last.ckpt'
